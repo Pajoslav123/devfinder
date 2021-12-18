4cfa82b5b4812170932d57159a3a706ab2e97e20
Pajorad Pajorad <pavle.maodus2004@gmail.com>
2021-12-18 01:20:35 +0100

    js fixes

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 
 //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
 
-function getUserInfo(){
-    var userName = document.getElementById('searchbar').value; 
+function getUserInfo(userName){
+    if(userName == null){
+        var userName = document.getElementById('searchbar').value; 
+    }
+    
     fetch('https://api.github.com/users/'+userName)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        document.getElementById("username").innerHTML = data.name;
-        document.getElementById("location").innerHTML = data.location;
-        document.getElementById("tag").innerHTML = "@" + data.login;
-        document.getElementById("date-joined").innerHTML = data.created_at;
-        document.getElementById("bio").innerHTML = data.bio;
+        if(data.name == null){
+            document.getElementById("username").textContent = data.login;
+        }
+        else
+            document.getElementById("username").textContent = data.name;
+        if(data.location == null){
+            document.getElementById("location").textContent = "Not available";
+        }
+        else
+            document.getElementById("location").textContent = data.location;
+
+        var date = new Date(data.created_at).toLocaleDateString('en-CA');
+        document.getElementById("tag").textContent = "@" + data.login;
+        document.getElementById("date-joined").textContent = "Date joined: " + date;
+        document.getElementById("bio").textContent = data.bio;
         document.getElementById("userAvatar").src = data.avatar_url;
-        document.getElementById("following").innerHTML = data.following;
-        document.getElementById("followers").innerHTML = data.followers;
-        document.getElementById("work").innerHTML = data.company;
-        document.getElementById("github").innerHTML = data.html_url;
+        document.getElementById("following").textContent = data.following;
+        document.getElementById("followers").textContent = data.followers;
+        if(data.company == null){
+            document.getElementById("work").textContent = "Not available";
+        }
+        else
+            document.getElementById("work").textContent = data.company;
+        document.getElementById("github").textContent = data.html_url;
         
         fetch(data.repos_url)
         .then(response => response.json())
-        .then(data => document.getElementById("repos").innerHTML = data.length);
+        .then(data => document.getElementById("repos").textContent = data.length);
         
     }
 );
